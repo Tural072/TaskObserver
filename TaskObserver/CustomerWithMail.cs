84b4150819079ff1d71fa85ce33c86ca1575f829
Tahi_ri96@itstep.edu.az <Tahi_ri96@itstep.edu.az>
2021-07-30 16:41:49 +0400

    Add project files.

---
+++ TaskObserver/CustomerWithMail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskObserver
+{
+    public class CustomerWithMail : ICustomer
+    {
+        public string ProductName { get; set; }
+        public void GetMessage()
+        {
+            Console.WriteLine("I am With Mail");
+        }
+    }
+}
